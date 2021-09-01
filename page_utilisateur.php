4e4ee7b09d68fdfaf5ec8c9b7ff6ff29c6514ee8
Khalifa <diadhiouababacar15@gmail.com>
2021-08-31 21:27:44 -1100

    complément du formulaire d'inscription

---
+++ page_utilisateur.php
@@ -0,0 +1,87 @@
+<?php
+    require 'header_insc.php';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="visit.css">
+    <title>page de visite</title>
+</head>
+<body>
+  <div class="container">
+            <div class="form-group">
+                <a href="sign_out.php"><button class="btn btn-danger form-control">Se Déconnecter</button></a>
+            </div>
+    </div>
+</nav>
+<section class="imageHotel">
+                <h1> Image Hotel </h1>
+                <h2> Proin iaculis purus consequat sem cure. </h2>
+                <br><br>
+                <div>
+                    <img src="hotel1.jpg" alt="">
+                    <div>
+                        <h3>Ebony & Ivory</h3>
+                        <p>Branding</p>
+                    </div>
+                </div>
+                <div>
+                    <img src="hotel2.jpg" alt="">
+                    <div>
+                        <h3>Smart Stationary</h3>
+                        <p>Print Design</p>
+                    </div>
+                </div>
+                <div>
+                    <img src="hotel3.jpg" alt="">
+                    <div>
+                        <h3>Clever Poster</h3>
+                        <p>Print Design</p>
+                    </div>
+                </div>
+                <div>
+                    <img src="hotel4.jpg" alt="">
+                    <div>
+                        <h3>Vinyl Record</h3>
+                        <p>Product Mock-up</p>
+                    </div>
+                </div>
+        </section>
+        <section class="imagePlage">
+                <h1> Image Plage </h1>
+                <h2> Proin iaculis purus consequat sem cure. </h2>
+                <br><br>
+                <div>
+                    <img src="plage1.jpg" alt="">
+                    <div>
+                        <h3>Ebony & Ivory</h3>
+                        <p>Branding</p>
+                    </div>
+                </div>
+                <div>
+                    <img src="plage2.jpg" alt="">
+                    <div>
+                        <h3>Smart Stationary</h3>
+                        <p>Print Design</p>
+                    </div>
+                </div>
+                <div>
+                    <img src="plage3.jpg" alt="">
+                    <div>
+                        <h3>Clever Poster</h3>
+                        <p>Print Design</p>
+                    </div>
+                </div>
+                <div>
+                    <img src="plage4.jpg" alt="">
+                    <div>
+                        <h3>Vinyl Record</h3>
+                        <p>Product Mock-up</p>
+                    </div>
+                </div>
+        </section>
+</body>
+</html>
